b680bae490e492d7fb05cae60d52f0f7fa9560c6
Tanmay-Soni <73430591+Tanmay-Soni@users.noreply.github.com>
2024-05-18 17:02:57 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,26 @@
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
-    { 
+    {     
         //Write your code here
+        // Perform binary search to find the index of element x in arr[l..r]
+        while (l <= r) {
+            // Calculate the middle index
+            int m = (l + (r - l)) / 2;
+            
+            // If element at middle index is equal to x, return its index
+            if (arr[m] == x) {
+                return m;
+            } 
+            // If x is smaller than the element at middle index, search in the left half
+            else if (x < arr[m]) {
+                r = m - 1; // Update right boundary
+            } 
+            // If x is greater than the element at middle index, search in the right half
+            else {
+                l = m + 1; // Update left boundary
+            }
+        }
     } 
   
     // Driver method to test above 
