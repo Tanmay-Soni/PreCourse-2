2cfdf777c9fedc4cc12fe0cce72feaac42b7ec6d
Tanmay-Soni <73430591+Tanmay-Soni@users.noreply.github.com>
2024-05-18 17:04:18 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,6 @@
+// Time Complexity : o(log(n))
+// Space Complexity : o(1)
+
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
